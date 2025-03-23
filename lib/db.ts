c94b343dc61281a8c436c72dd6e9771ba47d510b
clxrityy <clxrityxyz@gmail.com>
2025-03-23 14:17:56 -0400

    prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 function makePrisma() {
-    return new PrismaClient()
+  return new PrismaClient();
 }
 
 const globalForPrisma = global as unknown as {
-    prisma: ReturnType<typeof makePrisma>;
-}
+  prisma: ReturnType<typeof makePrisma>;
+};
 export const db = globalForPrisma.prisma ?? makePrisma();
 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = makePrisma();
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = makePrisma();
 }
