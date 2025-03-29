9aeffcb0dc9819446a4c639328dab336c8e3575a
clxrityy <clxrityxyz@gmail.com>
2025-03-29 13:03:50 -0400

    add session id

--- components/ui/wrappers/Main.tsx
+++ components/ui/wrappers/Main.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { SessionProvider, useSession } from '@/contexts/session';
 import { ReactNode } from 'react';
 
-export function Main({ children }: { children: ReactNode }) {
+export function Main({ children, sessionId }: { children: ReactNode; sessionId?: string }) {
+
+  const { setSessionId } = useSession();
+
+  if (sessionId) {
+    setSessionId(sessionId);
+  }
+
   return (
     <div className='w-full max-w-7xl mx-auto px-4 py-2 relative'>
-      {children}
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </div>
   );
 }
