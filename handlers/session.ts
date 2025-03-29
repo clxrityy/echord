ffdd6c3bc7ad832bfcba55375a293a3ea6717614
clxrityy <clxrityxyz@gmail.com>
2025-03-29 13:04:16 -0400

    prettier

--- handlers/session.ts
+++ handlers/session.ts
@@ -1,19 +1,19 @@
-import { db } from "@/lib/db";
-import { fetchIp } from "@/util/ip";
-import { v4 as uuidv4 } from "uuid";
+import { db } from '@/lib/db';
+import { fetchIp } from '@/utils/ip';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function handleCurrentSession() {
   const ip = await fetchIp();
   if (!ip) {
-    throw new Error("Failed to fetch IP");
+    throw new Error('Failed to fetch IP');
   }
   try {
     const currentSession = await db.eSession.findFirst({
       where: {
         ipAddresses: {
-          has: ip
+          has: ip,
         },
-      }
+      },
     });
 
     if (currentSession) {
@@ -22,8 +22,8 @@ export async function handleCurrentSession() {
       return await createSession();
     }
   } catch (e) {
-    console.error("Error fetching current session:", e);
-    throw new Error("Database error");
+    console.error('Error fetching current session:', e);
+    throw new Error('Database error');
   }
 }
 
@@ -31,7 +31,7 @@ export async function createSession() {
   const ip = await fetchIp();
 
   if (!ip) {
-    throw new Error("Failed to fetch IP or User-Agent");
+    throw new Error('Failed to fetch IP or User-Agent');
   }
 
   const sessionId = uuidv4();
@@ -41,28 +41,27 @@ export async function createSession() {
       data: {
         sessionId,
         ipAddresses: [ip],
-      }
+      },
     });
     return newSession;
   } catch (e) {
-    console.error("Error creating session:", e);
-    throw new Error("Database error");
+    console.error('Error creating session:', e);
+    throw new Error('Database error');
   }
-
 }
 
 export async function checkForExistingSession() {
   const ip = await fetchIp();
   if (!ip) {
-    throw new Error("Failed to fetch IP");
+    throw new Error('Failed to fetch IP');
   }
   try {
     const existing = await db.eSession.findFirst({
       where: {
         ipAddresses: {
-          has: ip
+          has: ip,
         },
-      }
+      },
     });
 
     if (existing) {
@@ -71,37 +70,35 @@ export async function checkForExistingSession() {
       return false;
     }
   } catch (e) {
-    console.error("Error checking for existing session:", e);
-    throw new Error("Database error");
+    console.error('Error checking for existing session:', e);
+    throw new Error('Database error');
   }
 }
-
 
 export async function deleteSession(id: string) {
   try {
     const deletedSession = await db.eSession.delete({
       where: {
-        sessionId: id
-      }
+        sessionId: id,
+      },
     });
     return deletedSession;
   } catch (e) {
-    console.error("Error deleting session:", e);
-    throw new Error("Database error");
+    console.error('Error deleting session:', e);
+    throw new Error('Database error');
   }
 }
-
 
 export async function getSessionById(id: string) {
   try {
     const session = await db.eSession.findUnique({
       where: {
-        sessionId: id
-      }
+        sessionId: id,
+      },
     });
     return session;
   } catch (e) {
-    console.error("Error fetching session by ID:", e);
-    throw new Error("Database error");
+    console.error('Error fetching session by ID:', e);
+    throw new Error('Database error');
   }
 }
