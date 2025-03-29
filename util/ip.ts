37cff34e64892ee0738f08cddc36f8166c508e44
clxrityy <clxrityxyz@gmail.com>
2025-03-29 13:04:56 -0400

    util/ -> utils/

--- util/ip.ts
+++
@@ -1,7 +0,0 @@
-import { IP_API_URL } from "@/lib/config";
-import axios from "axios";
-
-export async function fetchIp(): Promise<string> {
-  const { ip } = await axios.get(`${IP_API_URL}?format=json`).then(res => res.data);
-  return ip;
-}
---
+++ utils/ip.ts
@@ -0,0 +1,9 @@
+import { IP_API_URL } from '@/utils/constants';
+import axios from 'axios';
+
+export async function fetchIp(): Promise<string> {
+  const { ip } = await axios
+    .get(`${IP_API_URL}?format=json`)
+    .then((res) => res.data);
+  return ip;
+}
