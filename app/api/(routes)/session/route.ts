11afff6324602b84140d86dbdea1398c2a4e7bc4
clxrityy <clxrityxyz@gmail.com>
2025-03-29 13:03:03 -0400

    prettier

--- app/api/(routes)/session/route.ts
+++ app/api/(routes)/session/route.ts
@@ -1,16 +1,21 @@
-import { checkForExistingSession, createSession, deleteSession, getSessionById } from "@/handlers/session";
-import { ENV } from "@/lib/config";
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import { sign, verify } from "jsonwebtoken";
-import { serialize } from "cookie";
+import {
+  checkForExistingSession,
+  createSession,
+  deleteSession,
+  getSessionById,
+} from '@/handlers/session';
+import { ENV } from '@/utils/constants';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { sign, verify } from 'jsonwebtoken';
+import { serialize } from 'cookie';
 
-export async function POST(req: NextRequest) {
+export async function POST(_req: NextRequest) {
   const cookieStore = await cookies();
   const isExisting = await checkForExistingSession();
 
   if (isExisting) {
-    return NextResponse.json("Session already exists", { status: 409 });
+    return NextResponse.json('Session already exists', { status: 409 });
   }
 
   const cookie = cookieStore.get(ENV.COOKIE_NAME);
@@ -18,7 +23,7 @@ export async function POST(req: NextRequest) {
   if (cookie?.value) {
     const decoded = verify(cookie.value, ENV.JWT_SECRET);
 
-    if (decoded && typeof decoded === "object" && "sessionId" in decoded) {
+    if (decoded && typeof decoded === 'object' && 'sessionId' in decoded) {
       const sessionId = (decoded as { sessionId: string }).sessionId;
 
       // Check if the session is still valid
@@ -36,44 +41,61 @@ export async function POST(req: NextRequest) {
   const session = await createSession();
 
   if (!session) {
-    return NextResponse.json({ error: "Failed to create session" }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to create session' },
+      { status: 500 }
+    );
   }
 
-  const token = sign({ sessionId: session.sessionId }, ENV.JWT_SECRET, { expiresIn: "72h" });
+  const token = sign({ sessionId: session.sessionId }, ENV.JWT_SECRET, {
+    expiresIn: '72h',
+  });
   try {
-    cookieStore.set(ENV.COOKIE_NAME, serialize(ENV.COOKIE_NAME, token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-    }));
+    cookieStore.set(
+      ENV.COOKIE_NAME,
+      serialize(ENV.COOKIE_NAME, token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+      })
+    );
 
     return NextResponse.json({ sessionId: session.sessionId }, { status: 200 });
   } catch (e) {
-    console.error("Error setting cookie:", e);
+    console.error('Error setting cookie:', e);
     try {
       await deleteSession(session.sessionId);
-      return NextResponse.json({ error: "Failed to set cookie" }, { status: 500 });
+      return NextResponse.json(
+        { error: 'Failed to set cookie' },
+        { status: 500 }
+      );
     } catch (e) {
-      console.error("Error deleting session:", e);
-      return NextResponse.json({ error: "Failed to delete invalid session from database" }, { status: 500 });
+      console.error('Error deleting session:', e);
+      return NextResponse.json(
+        { error: 'Failed to delete invalid session from database' },
+        { status: 500 }
+      );
     }
   }
 }
 
-export async function GET(req: NextRequest) {
+export async function GET(_req: NextRequest) {
   const cookieStore = await cookies();
 
   const token = cookieStore.get(ENV.COOKIE_NAME)?.value;
 
   if (!token) {
-    return NextResponse.json({ error: "No session found" }, { status: 401 });
+    return NextResponse.json({ error: 'No session found' }, { status: 401 });
   } else {
     try {
       const decoded = verify(token, ENV.JWT_SECRET);
-      return NextResponse.json({ sessionId: (decoded as { sessionId: string }).sessionId }, { status: 200 });
+      return NextResponse.json(
+        { sessionId: (decoded as { sessionId: string }).sessionId },
+        { status: 200 }
+      );
     } catch (e) {
-      console.error("Error verifying token:", e);
-      return NextResponse.json({ error: "Invalid session" }, { status: 401 });
+      console.error('Error verifying token:', e);
+      return NextResponse.json({ error: 'Invalid session' }, { status: 401 });
     }
   }
 }
