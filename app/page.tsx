6ba52dce678892a91e55a85bfeda1e4342fea325
clxrityy <clxrityxyz@gmail.com>
2025-03-31 16:06:19 -0400

    begin feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import { handleCurrentSession } from "@/handlers/session";
+import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
 
+  const session = await handleCurrentSession();
 
+  const feed = await db.eInteraction.findMany({
+    orderBy: {
+      createdAt: "desc",
+    }
+  });
+
+  
   return <main className="w-full h-full relative mt-20">
 
   </main>;
