8b8ff4bf938b8f61bd4088d9ad13d681ec735059
clxrityy <clxrityxyz@gmail.com>
2025-04-04 18:18:46 -0400

    add search handler

---
+++ app/api/(routes)/session/search/route.ts
@@ -0,0 +1,31 @@
+import { addSearchToUser } from "@/handlers/search";
+import { getUserBySessionId } from "@/handlers/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { search, sessionId } = await req.json();
+
+  if (!search || !sessionId) {
+    return NextResponse.json("Missing search or sessionId", { status: 400 });
+  }
+
+  const user = await getUserBySessionId(sessionId);
+
+  if (!user) {
+    return NextResponse.json("User not authorized", { status: 401 });
+  }
+
+  try {
+    const newSearch = await addSearchToUser(sessionId, search);
+
+    if (newSearch) {
+      return NextResponse.json(newSearch, { status: 200 });
+    } else {
+      return NextResponse.json("Error creating new search", { status: 500 });
+    }
+
+  } catch (e) {
+    console.error("Error adding search to user:", e);
+    return NextResponse.json("Database error", { status: 500 });
+  }
+}
---
+++ handlers/search/index.ts
@@ -0,0 +1,59 @@
+import { db } from "@/lib/db";
+import { getUserBySessionId } from "../user";
+
+export async function addSearchToUser(sessionId: string, search: string) {
+  const user = await getUserBySessionId(sessionId);
+
+  if (!user) {
+    return undefined;
+  }
+
+  const userWithSearches = await db.eUser.findUnique({
+    where: {
+      id: user.id
+    },
+    include: {
+      searches: true,
+    }
+  });
+
+  if (!userWithSearches) {
+    return undefined;
+  }
+
+  const doesSearchExist = userWithSearches.searches.some((s) => s.searchQuery === search);
+
+  if (!doesSearchExist) {
+    try {
+      const newSearch = await db.eSearch.create({
+        data: {
+          searchQuery: search,
+          userId: user.userId,
+        }
+      });
+
+      if (newSearch) {
+        await db.eUser.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            searches: {
+              connect: {
+                id: newSearch.id,
+              },
+            },
+          },
+        });
+        return newSearch;
+      } else {
+        console.error('Error creating new search:', newSearch);
+        throw new Error('Database error');
+      }
+
+    } catch (e) {
+      console.error('Error adding search to user:', e);
+      throw new Error('Database error');
+    }
+  }
+}
