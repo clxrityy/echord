17b4ab691b3790acff90b4b07eef3f31d096341f
clxrityy <clxrityxyz@gmail.com>
2025-04-08 04:04:18 -0400

    new hook

---
+++ hooks/useScreenSize.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+
+export const useScreenSize = () => {
+
+  if (typeof window === "undefined") {
+    return {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = useCallback(() => {
+    setScreenSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  useEffect(() => {
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenSize;
+}
