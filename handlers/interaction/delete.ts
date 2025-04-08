f6e093f1c268513be44e4dfd86b01396bdcfa931
clxrityy <clxrityxyz@gmail.com>
2025-04-08 04:04:59 -0400

    add delete

---
+++ handlers/interaction/delete.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db";
+
+export type DeleteInteractionProps = {
+  userId: string;
+  interactionId: string;
+}
+
+export async function deleteInteraction(
+  { userId, interactionId }: DeleteInteractionProps
+) {
+  const existing = await db.eInteraction.findFirst({
+    where: {
+      dataId: interactionId,
+      userId: userId,
+    }
+  });
+
+  if (!existing) {
+    throw new Error("Interaction not found");
+  }
+
+  try {
+    await db.eInteraction.delete({
+      where: {
+        id: existing.id,
+      },
+    });
+  } catch (e) {
+    throw new Error("Failed to delete interaction");
+  }
+}
