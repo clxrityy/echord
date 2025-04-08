b47b2ff27516501a15f5a24a61082489443be200
clxrityy <clxrityxyz@gmail.com>
2025-04-08 13:54:23 -0400

    hero (begin)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { FeedItem } from "@/components/elements/feed/Feed";
 import { FeedList, FeedListItem } from "@/components/elements/feed/FeedList";
+import { Hero } from "@/components/elements/Hero";
 import { Window } from "@/components/layout/screen/Window";
 import Skeleton from "@/components/ui/Skeleton";
 import { handleCurrentSession } from "@/handlers/session";
@@ -22,11 +23,12 @@ export default async function Home() {
   });
 
 
-  return <main className="w-full h-full relative items-center justify-center mx-auto flex flex-col gap-10">
+  return <main className="w-full h-full relative items-center justify-center mx-auto flex flex-col gap-10 mt-26 xl:mt-10 2xl:mt-0 overflow-y-scroll xl:overflow-clip scroll-smooth">
     {/**
      *
      */}
-    <div className="w-full h-full relative flex flex-col xl:flex-row gap-5 items-start justify-start xl:justify-between xl:items-start xl:gap-0">
+    <div className="w-full h-full xl:h-[90vh] relative flex flex-col xl:flex-row gap-5 items-start justify-start xl:justify-between xl:items-start xl:gap-0">
+      <Hero userId={session.userId || undefined} />
       <Suspense fallback={<Skeleton className="w-full h-full" />}>
         <Window sessionId={session.userId || ""}>
           <div className="relative flex justify-center items-center w-full">
