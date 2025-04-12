2d7b3d6e118d49c092a139d157506ea592118fb0
clxrityy <clxrityxyz@gmail.com>
2025-04-12 15:36:24 -0400

    begin album component

---
+++ components/elements/data/Album.tsx
@@ -0,0 +1,21 @@
+import { EAlbum } from "@prisma/client"
+
+type Props = {
+  album: EAlbum;
+}
+
+export function Album({
+  album
+}: Props) {
+  if (!album) {
+    return <h1 className="">Album not found</h1>;
+  }
+
+  return (
+    <div className="w-full h-full">
+      <h1>
+        {album.title} by {album.artistName}
+      </h1>
+    </div>
+  )
+}
