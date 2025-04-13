be3c50a58dba70fe0a1a293a3823fc71e794411e
clxrityy <clxrityxyz@gmail.com>
2025-04-13 16:15:28 -0400

    border

--- components/elements/user/Favorites.tsx
+++ components/elements/user/Favorites.tsx
@@ -20,7 +20,7 @@ export const Favorites = ({ interactionData }: FavoritesProps) => {
       <h1 className="text-2xl font-bold mb-4 flex flex-row gap-2 items-center">
        <ICONS.favorite />
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 border border-white/20 rounded-lg p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 border border-white/10 rounded-lg p-4">
         {interactionData.map((data) => (
           <div key={data.id} className="shadow-md rounded-lg p-4 flex flex-col items-center">
             {
