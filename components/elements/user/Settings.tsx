d3482a4930d45e511a9a1597ea741235f4b7595a
clxrityy <clxrityxyz@gmail.com>
2025-04-13 21:17:15 -0400

    move button outside of outsideclick

--- components/elements/user/Settings.tsx
+++ components/elements/user/Settings.tsx
@@ -4,7 +4,7 @@ import { ICONS } from "@/utils/constants";
 import { useRef, useState } from "react";
 import { OutsideClick } from "@/components/ui/wrappers/OutsideClick";
 
-export function Settings(){
+export function Settings() {
 
   const [clicked, setClicked] = useState<boolean>(false);
 
@@ -12,13 +12,19 @@ export function Settings(){
     setClicked((prev) => !prev);
   };
 
+  const handleOutsideClick = () => {
+    setClicked(false);
+  }
+
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   return (
-    <button role="button" onClick={handleClick} className="transition duration-200 focus:outline-none rounded-md hover:text-gray-400 focus:text-blue-400">
-      <span className="sr-only">Settings</span>
-      <ICONS.settings className="h-12 w-12" />
-      <OutsideClick onOutsideClick={() => setClicked(false)}>
+    <>
+      <button type="button" title="settings" role="button" onClick={handleClick} className="transition duration-200 focus:outline-none rounded-md hover:text-gray-400 focus:text-blue-400">
+        <span className="sr-only">Settings</span>
+        <ICONS.settings className="h-12 w-12" />
+      </button>
+      <OutsideClick onOutsideClick={handleOutsideClick}>
         <dialog ref={dialogRef} className={`${clicked ? 'block' : 'hidden'} absolute top-20 right-0 w-3/4 h-[40vh] bg-black/35 z-10 rounded-lg shadow-lg backdrop-blur-md`}>
           <div className="flex flex-col h-full text-white items-center justify-start w-full pt-2 relative">
             <h2>
@@ -30,6 +36,6 @@ export function Settings(){
           </div>
         </dialog>
       </OutsideClick>
-    </button>
+    </>
   )
 }
