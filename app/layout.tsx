63662d52c1fc13adef0f54dce1aeb9b86965eae7
clxrityy <clxrityxyz@gmail.com>
2025-04-13 21:17:03 -0400

    move this for whatever reason

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,11 +97,6 @@ export default async function RootLayout({
       <head>
         <link rel='icon' href='/favicon.ico' />
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
           rel='icon'
           type='image/png'
           sizes='32x32'
