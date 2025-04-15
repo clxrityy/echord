7e98985e32b0436e789000c3804dd18ad6c31d0f
clxrityy <clxrityxyz@gmail.com>
2025-04-15 00:51:48 -0400

    add modal & user agent

--- contexts/window/index.tsx
+++ contexts/window/index.tsx
@@ -1,12 +1,23 @@
 'use client';
 import { useContextFactory } from '@/hooks/useContextFactory';
 import { useWindowStore } from './useWindow';
+import { EUserAgent } from '@/prisma/app/generated/prisma/client';
 
 export type WindowContextState = {
   width: number;
   height: number;
   setWindowSize: (width: number, height: number) => void;
   getWindowSize: () => { width: number; height: number };
+  hasOpenModal: boolean;
+  setCurrentOpenModal: (title: string) => void;
+  currentOpenModal: {
+    title: string;
+  } | null;
+  closeModal: () => void;
+  getCurrentOpenModal: () => { title: string } | null;
+  userAgent: Partial<EUserAgent> | null;
+  setUserAgent: (userAgent: Partial<EUserAgent>) => void;
+  getUserAgent: () => Partial<EUserAgent> | null;
 };
 
 const initialWindowContextState: WindowContextState = {
@@ -14,6 +25,14 @@ const initialWindowContextState: WindowContextState = {
   height: 0,
   setWindowSize: () => {},
   getWindowSize: () => ({ width: 0, height: 0 }),
+  hasOpenModal: false,
+  setCurrentOpenModal: () => {},
+  currentOpenModal: null,
+  closeModal: () => {},
+  getCurrentOpenModal: () => null,
+  userAgent: null,
+  setUserAgent: () => {},
+  getUserAgent: () => null,
 };
 
 export const { Provider: WindowProvider, useContext: useWindow } =
