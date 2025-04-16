65451c274da54e33308b1451ea77375c43f8ac1c
clxrityy <clxrityxyz@gmail.com>
2025-04-16 10:12:21 -0400

    dialog component

---
+++ components/ui/Dialog.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useRef, useEffect, ReactNode, ComponentProps } from "react";
+import { Button } from "./Button";
+import { ICONS } from "@/utils";
+
+export interface DialogProps extends ComponentProps<"dialog"> {
+  title: string;
+  description?: string;
+  onClose: () => void;
+  open: boolean;
+}
+
+export function Dialog({
+  title,
+  description,
+  onClose,
+  open,
+  children,
+  ...props
+}: DialogProps & {
+  children: ReactNode;
+}) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [open]);
+
+  const closeDialog = () => {
+    dialogRef.current?.close();
+    onClose();
+  }
+
+  return <dialog ref={dialogRef} {...props} className="fixed top-50 left-50 -translate-x-45 -translate-y-0 z-10  rounded-xl backdrop:bg-gray-800/50">
+    <div className="w-[400px] max-w-full flex flex-col">
+      <div className="flex items-center justify-between p-4 border-b border-gray-200/20 rounded-sm">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <Button onClick={closeDialog}>
+          <ICONS.close className="h-4 w-4" />
+        </Button>
+      </div>
+      <div>
+        {description && <p className="p-4 text-gray-600">{description}</p>}
+        <div className="p-4">
+          {children}
+        </div>
+      </div>
+    </div>
+  </dialog>
+}
