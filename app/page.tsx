aa2aa351d5e5d204dde3dcd8155c54d8f2ce8637
clxrityy <clxrityxyz@gmail.com>
2025-04-18 19:35:33 -0400

    rearrange window

--- app/page.tsx
+++ app/page.tsx
@@ -35,51 +35,52 @@ export default async function Home() {
        *
        */}
       <div className='w-full h-full xl:h-[100vh] flex flex-col xl:flex-row gap-2 items-start justify-start xl:justify-between xl:items-start xl:gap-0'>
-        <Hero userId={session.userId || undefined} />
         <Suspense fallback={<Skeleton className='w-full h-full animate-pulse bg-gray-500/50' />}>
-          <Window sessionId={session.userId || ''}>
-            <div className='relative flex justify-center items-center w-full'>
-              <div className='w-full h-fit flex items-center justify-center relative pb-20'>
-                <FeedList itemsPerPage={FEED_ITEMS_PER_PAGE}>
-                  {Array.from(rawFeed).map(async (item, idx) => {
-                    const { ...interaction } = item;
-                    const { ...data } = await db.eData.findUnique({
-                      where: {
-                        id: item.dataId,
-                      },
-                    });
-                    const { ...interactionData } =
-                      await db.eInteractionData.findFirst({
-                        where: {
-                          dataId: item.dataId,
-                        },
-                      });
-
-                    const user = await getUserBySessionId(data.sessionId);
-
-                    return (
-                      <Suspense key={idx} fallback={<FeedListItemSkeleton />}>
-                        <FeedListItem key={idx}>
-                          {interaction && data && interactionData ? (
-                            <FeedItem
-                              interaction={interaction}
-                              data={data}
-                              interactionData={interactionData}
-                              userId={session.userId}
-                              username={user?.username ? user.username : null}
-                            />
-                          ) : (
-                            <Skeleton className='w-full h-20 rounded-md' />
-                          )}
-                        </FeedListItem>
-                      </Suspense>
-                    );
-                  })}
-                </FeedList>
-              </div>
-            </div>
+          <Window sessionId={session!.userId ?? session?.userId!}>
+            <Hero userId={session!.userId ?? session?.userId!} />
           </Window>
         </Suspense>
+        <div className='relative flex justify-center items-center w-full mt-20'>
+          <div className='w-full h-fit flex items-center justify-center relative pb-20'>
+            <FeedList itemsPerPage={FEED_ITEMS_PER_PAGE}>
+              {Array.from(rawFeed).map(async (item, idx) => {
+                const { ...interaction } = item;
+                const { ...data } = await db.eData.findUnique({
+                  where: {
+                    id: item.dataId,
+                  },
+                });
+                const { ...interactionData } =
+                  await db.eInteractionData.findFirst({
+                    where: {
+                      dataId: item.dataId,
+                    },
+                  });
+
+                const user = await getUserBySessionId(data.sessionId);
+
+                return (
+                  <Suspense key={idx}>
+                    <FeedListItem>
+                      {interaction && data && interactionData ? (
+                        <FeedItem
+                          interaction={interaction}
+                          data={data}
+                          interactionData={interactionData}
+                          userId={session!.userId ?? session?.userId!}
+                          username={user?.username ? user.username : null}
+                        />
+                      ) : (
+                        <Skeleton className='w-full h-20 rounded-md' />
+                      )}
+                    </FeedListItem>
+                  </Suspense>
+                );
+              })}
+            </FeedList>
+          </div>
+        </div>
+
       </div>
     </main>
   );
