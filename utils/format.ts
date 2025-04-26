88675c87a65cb70e37bf611cd25b33e241b25f48
clxrityy <clxrityxyz@gmail.com>
2025-04-26 14:55:55 -0400

    remove unnecessary function

--- utils/format.ts
+++ utils/format.ts
@@ -1,7 +1,3 @@
 export function capitalizeFirstLetter(input: string): string {
   return input.charAt(0).toUpperCase() + input.slice(1);
 }
-
-export function replaceUrlEncodeWithSpace(input: string): string {
-  return input.replace(/%20/g, ' ');
-}
