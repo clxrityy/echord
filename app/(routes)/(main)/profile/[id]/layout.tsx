61dc6aa50ec7ffcaac465ee6f955e216a5da51fb
clxrityy <clxrityxyz@gmail.com>
2025-04-29 14:28:56 -0400

    add layout

---
+++ app/(routes)/(main)/profile/[id]/layout.tsx
@@ -0,0 +1,66 @@
+import { db } from "@/lib";
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = (await params).id;
+
+  const profileUser = await db.eUser.findUnique({
+    where: {
+      userId: id,
+    },
+  });
+
+  if (!profileUser) {
+    return {
+      title: "User not found",
+    };
+  }
+
+  return {
+    title: `${profileUser.username} | Echord`,
+    description: `Profile page of ${profileUser.username}`,
+    openGraph: {
+      title: `Echord | ${profileUser.username}`,
+      description: `Profile page of ${profileUser.username}`,
+      url: `https://echord.com/profile/${profileUser.userId}`,
+      images: [
+        {
+          url: profileUser.avatar || "https://echord.com/img/default-avatar.png",
+          width: 250,
+          height: 250,
+        },
+      ],
+    },
+    twitter: {
+      title: `Echord | ${profileUser.username}`,
+      description: `Profile page of ${profileUser.username}`,
+      images: [
+        {
+          url: profileUser.avatar || "https://echord.com/img/default-avatar.png",
+          width: 250,
+          height: 250,
+        },
+      ],
+      card: "summary_large_image",
+    },
+  };
+}
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <div className="w-full h-full relative">
+      {children}
+    </div>
+  );
+}
