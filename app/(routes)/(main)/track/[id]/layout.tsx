6dfd404b54192393d76af9930d72dfaffc8d61a9
clxrityy <clxrityxyz@gmail.com>
2025-04-28 20:51:43 -0400

    track route

---
+++ app/(routes)/(main)/track/[id]/layout.tsx
@@ -0,0 +1,97 @@
+import { checkTrackFromInteraction } from "@/app/_actions";
+import { Metadata } from "next";
+import { connection } from "next/server";
+import { ReactNode } from "react";
+
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = (await params).id;
+
+  const track = await checkTrackFromInteraction(id);
+
+  if (!track) {
+    return {
+      title: "Track not found",
+      description: "Track not found",
+    };
+  }
+
+  return {
+    title: `${track.title} by ${track.artistName}`,
+    keywords: [
+      "Echord",
+      "Track",
+      "Music",
+      "Music Track",
+      `${track.title}`,
+      `${track.artistName}`,
+      `${track.title} by ${track.artistName}`,
+      `${track.artistName} Track`,
+      `${track.title} Track`,
+      `${track.title} by ${track.artistName} Track`,
+      `${track.title} by ${track.artistName} Music`,
+      `${track.artistName} Music`,
+    ],
+    description: `${track.title} by ${track.artistName}`,
+    openGraph: {
+      title: track.title,
+      description: `${track.title} by ${track.artistName}`,
+      url: `https://echord.uk/track/${id}`,
+    },
+    twitter: {
+      title: track.title,
+      description: `${track.title} by ${track.artistName}`,
+      card: "summary",
+      images: [
+        {
+          url: track.imageUrl!,
+          alt: `${track.title} by ${track.artistName}`,
+          width: 400,
+          height: 400,
+        },
+      ],
+    },
+    icons: {
+      icon: track.imageUrl!,
+      shortcut: track.imageUrl!,
+      apple: track.imageUrl!,
+    },
+    alternates: {
+      canonical: `https://echord.uk/track/${id}`,
+    },
+    metadataBase: new URL("https://echord.uk"),
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      noarchive: false,
+      noimageindex: false,
+      notranslate: false,
+    },
+    appleWebApp: {
+      title: `Echord | ${track.title}`,
+      statusBarStyle: "black-translucent",
+      capable: true,
+    },
+    themeColor: "#000000"
+  };
+}
+
+export default async function TrackLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  await connection();
+
+  return (
+    <div className="w-full h-full mt-30 relative">
+      {children}
+    </div>
+  );
+}
---
+++ app/(routes)/(main)/track/[id]/page.tsx
@@ -0,0 +1,67 @@
+import Loading from "@/app/loading";
+import { Window } from "@/components/layout";
+import { ImageComponent } from "@/components/ui";
+import { db, getUserSessionId } from "@/lib";
+import { connection } from "next/server";
+import { Suspense } from "react";
+
+
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function Page({ params }: Props) {
+  await connection();
+
+  const sessionId = await getUserSessionId();
+  const id = (await params).id;
+
+  const track = await db.eTrack.findUnique({
+    where: {
+      trackId: id,
+    },
+  });
+
+  if (!track) {
+    return <h1 className="">Track not found</h1>;
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Window sessionId={sessionId || ""}>
+        <div className="w-full h-full mx-auto flex items-center justify-start">
+          <div className="flex flex-col w-full h-full mx-auto gap-6 items-center justify-center">
+            <h2>Track</h2>
+            <div className="flex flex-col w-full h-full mx-auto gap-6 items-center justify-center">
+              <div className="flex flex-row w-full h-full mx-auto gap-6 items-center justify-center">
+                {track.imageUrl && (
+                  <ImageComponent
+                    src={track.imageUrl}
+                    alt={track.title}
+                    className="w-full h-full object-cover rounded-lg"
+                    width={500}
+                    height={500}
+                    loading="eager"
+                    style={{ maxWidth: "100%", height: "auto" }}
+                    priority
+                    placeholder="blur"
+                    blurDataURL={track.imageUrl}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    quality={100}
+                  />
+                )}
+                <h2 className="text-2xl font-bold">{track.title}</h2>
+              </div>
+              <div className="flex flex-col w-full h-full mx-auto gap-6 items-center justify-center">
+                <h2 className="text-2xl font-bold">{track.artistName}</h2>
+                <h2 className="text-2xl font-bold">{track.albumName}</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Window>
+    </Suspense>
+  )
+}
