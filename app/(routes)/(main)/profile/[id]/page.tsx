1fd70f9dededa3652903dbfa1c24b35eb8511b84
clxrityy <clxrityxyz@gmail.com>
2025-04-29 14:30:58 -0400

    add userbox

--- app/(routes)/(main)/profile/[id]/page.tsx
+++ app/(routes)/(main)/profile/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
-import { SavesGrid, Favorites } from '@/components/categories/user';
+import { SavesGrid, Favorites, UserBox } from '@/components/categories/user';
 import { Window } from '@/components/layout';
 import Loading from '@/app/loading';
 import { db, getUserSessionId } from '@/lib';
 import { getUserBySessionId } from '@/app/_actions';
 import { SettingsModal } from '@/components/categories/modals';
+import { Skeleton } from '@/components/ui';
 
 type Props = {
   params: Promise<{
@@ -25,16 +26,32 @@ export default async function Page({ params }: Props) {
   const user = await getUserBySessionId(sessionId || '');
   const isCurrentUser = user?.userId === profileUser?.userId;
 
+  if (isCurrentUser) {
+    try {
+      await db.eUser.update({
+        where: {
+          userId: user?.userId,
+        },
+        data: {
+          updatedAt: new Date(),
+        }
+      })
+    } catch (e) {
+      console.error('Error updating user last seen:', e);
+    }
+  }
+
   if (!profileUser) {
     return <h1 className='mt-30'>User not found</h1>;
   }
 
   const getFavorites = async () => {
+
     const allFavorites = await db.eInteractionData.findMany({
       where: {
         interactionType: 'FAVORITED',
-      },
-    });
+      }
+    })
 
     const userFavorites = await db.eInteraction.findMany({
       where: {
@@ -43,21 +60,20 @@ export default async function Page({ params }: Props) {
       },
     });
 
-    const favorites = allFavorites.filter((favorite) =>
-      userFavorites.some(
-        (userFavorite) => userFavorite.dataId === favorite.dataId
-      )
-    );
+    const favorites = allFavorites.filter((favorite) => {
+      return userFavorites.some((userFavorite) => userFavorite.dataId === favorite.dataId);
+    })
 
     return favorites;
   };
 
   const getSaves = async () => {
+
     const allSaves = await db.eInteractionData.findMany({
       where: {
         interactionType: 'SAVED',
-      },
-    });
+      }
+    })
 
     const userSaves = await db.eInteraction.findMany({
       where: {
@@ -66,24 +82,58 @@ export default async function Page({ params }: Props) {
       },
     });
 
-    const saves = allSaves.filter((save) =>
-      userSaves.some((userSave) => userSave.dataId === save.dataId)
-    );
+    const saves = allSaves.filter((save) => {
+      return userSaves.some((userSave) => userSave.dataId === save.dataId);
+    })
 
     return saves;
   };
 
+  const getReviews = async () => {
+
+    const allReviews = await db.eInteractionData.findMany({
+      where: {
+        interactionType: 'REVIEWED',
+      }
+    })
+
+    const userReviews = await db.eInteraction.findMany({
+      where: {
+        userId: profileUser.userId,
+        interactionType: 'REVIEWED',
+      },
+    });
+
+    const reviews = allReviews.filter((review) => {
+      return userReviews.some((userReview) => userReview.dataId === review.dataId);
+    })
+
+    return reviews;
+  }
+
+
+  const counts = {
+    saves: (await getSaves()).length,
+    favorites: (await getFavorites()).length,
+    reviews: (await getReviews()).length,
+    followers: 0,
+    following: 0,
+  }
+
   return (
     <Suspense fallback={<Loading />}>
       <Window sessionId={sessionId || ''}>
-        <div className='w-full flex flex-col gap-10 items-center justify-around'>
+        <div className='w-full h-full relative'>
+          <Suspense fallback={<Skeleton className='w-[200px] h-[400px] animate-pulse drop-shadow-2xl shadow-2xl bg-gray-600/40 rounded-lg' />}>
+            <UserBox user={profileUser} counts={counts} />
+          </Suspense>
           <div className='flex items-center justify-center gap-2 w-fit'>
-            <h1 className='text-2xl font-bold'>{profileUser.username}</h1>
+            {/* <h1 className='text-2xl font-bold'>{profileUser.username}</h1>
             {isCurrentUser && (
               <Suspense>
                 <SettingsModal userId={profileUser.userId} />
               </Suspense>
-            )}
+            )} */}
             {/**
              * - Profile data (bio, etc.)
              */}
---
+++ components/categories/user/UserBox.tsx
@@ -0,0 +1,81 @@
+import { ImageComponent } from "@/components/ui";
+import { EUser } from "@/prisma/app/generated/prisma/client"
+
+type Props = {
+  user: EUser;
+  counts: {
+    followers: number;
+    following: number;
+    reviews: number;
+    favorites: number;
+    saves: number;
+  }
+}
+
+export function UserBox({
+  user,
+  counts
+}: Props) {
+
+  const { followers, following, reviews, favorites, saves } = counts;
+
+  return (
+    <div className="absolute top-2 left-2 px-4 py-2 h-[100px]">
+      <div className="flex flex-col items-center justify-start gap-4 w-[200px] h-[200px] rounded-lg shadow-md">
+        <div className="flex items-center justify-center gap-4 w-full">
+          <ImageComponent
+            src={user.avatar || "/img/default-avatar.png"}
+            alt="User Avatar"
+            width={50}
+            height={50}
+            className="rounded-3xl w-12 h-12 object-cover"
+          />
+          <div className="flex flex-col items-start justify-start">
+            <h4 className="text-lg xl:text-xl text-gray-200 font-semibold">
+              {user.username}
+            </h4>
+            <p className="text-sm text-gray-300">
+              Last seen: <span className="text-gray-400">
+                {user.updatedAt.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </span>
+            </p>
+          </div>
+        </div>
+        <ul className="flex flex-col items-start justify-start gap-2 w-full h-full">
+          <li className="flex items-center justify-between w-full">
+            <span className="text-sm xl:text-base text-gray-300">Reviews</span>
+            <span className="text-sm text-gray-400">{reviews}</span>
+          </li>
+          <li className="flex items-center justify-between w-full">
+            <span className="text-sm xl:text-base text-gray-300">Favorites</span>
+            <span className="text-sm text-gray-400">{favorites}</span>
+          </li>
+          <li className="flex items-center justify-between w-full">
+            <span className="text-sm xl:text-base text-gray-300">Saves</span>
+            <span className="text-sm xl:text-base text-gray-400">{saves}</span>
+          </li>
+        </ul>
+      </div>
+      <div className="relative w-full h-[100%]">
+        <div className="absolute bottom-0 left-0 w-full bg-gray-700/25 rounded-b-lg drop-shadow-2xl shadow-md">
+          <div className="flex flex-col items-start justify-end gap-2 w-full">
+            <div className="flex items-center justify-between px-4 py-2 gap-2">
+              <span className="text-sm xl:text-base text-gray-300">Followers</span>
+              <span className="text-sm xl:text-base text-gray-400">{followers}</span>
+            </div>
+            <div className="flex items-center justify-between gap-2 px-4 py-2">
+              <span className="text-sm xl:text-base text-gray-300">Following</span>
+              <span className="text-sm xl:text-base text-gray-400">{following}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
