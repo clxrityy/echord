2e2337e1eb0f9f1f8dcbe8aaa23c83c7fa5de077
clxrityy <clxrityxyz@gmail.com>
2025-04-30 12:14:39 -0400

    add DELETE

--- app/(routes)/api/(routes)/interaction/route.ts
+++ app/(routes)/api/(routes)/interaction/route.ts
@@ -1,4 +1,4 @@
-import { db } from '@/lib';
+import { db, getUserSessionId } from '@/lib';
 import { Interaction } from '@/types';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -50,6 +50,69 @@ export async function GET(req: NextRequest) {
   return NextResponse.json(
     {
       interaction: data,
+    },
+    { status: 200 }
+  );
+}
+
+export async function DELETE(req: NextRequest) {
+  const sessionId = await getUserSessionId();
+
+  const { searchParams } = new URL(req.url);
+
+  const interactionId = searchParams.get('interactionId');
+  const userId = searchParams.get('userId');
+
+  if (!interactionId || !userId) {
+    return NextResponse.json(
+      {
+        error: 'Interaction ID and User ID are required',
+      },
+      { status: 400 }
+    );
+  }
+
+  const existing = await db.eInteraction.findFirst({
+    where: {
+      dataId: interactionId,
+      userId: userId,
+    },
+  });
+
+  if (!existing) {
+    return NextResponse.json(
+      {
+        error: 'Interaction not found',
+      },
+      { status: 404 }
+    );
+  }
+
+  try {
+    await db.eInteraction.delete({
+      where: {
+        id: existing.id,
+      },
+    });
+
+    await db.eInteractionData.delete({
+      where: {
+        dataId: existing.dataId,
+      }
+    })
+  } catch (e) {
+    console.error('Error deleting interaction:', e);
+    return NextResponse.json(
+      {
+        error: 'Failed to delete interaction',
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      message: 'Interaction deleted successfully',
     },
     { status: 200 }
   );
