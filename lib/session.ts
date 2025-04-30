f7c6b37f3e25d716edd889b68e5b3147e47b7b7b
clxrityy <clxrityxyz@gmail.com>
2025-04-30 14:32:03 -0400

    set httpOnly to false?

--- lib/session.ts
+++ lib/session.ts
@@ -95,7 +95,7 @@ export async function updateUserSession(request: NextRequest) {
     name: ENV.COOKIE_NAME,
     value: await encryptJWT(parsed),
     expires,
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
     maxAge: 60 * 60 * 24 * 7, // 7 days
   });
