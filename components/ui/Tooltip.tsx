8f076c00084dcaaf4d9d9ab98b315b526e3cfcb3
clxrityy <clxrityxyz@gmail.com>
2025-04-30 14:03:56 -0400

    height auto

--- components/ui/Tooltip.tsx
+++ components/ui/Tooltip.tsx
@@ -26,7 +26,7 @@ export function Tooltip({
     >
       {children}
       {isVisible && (
-        <div className='absolute top-[calc(100%+0.35rem)] transform-[translateX(-45%)] bg-gray-950/55 text-zinc-100/90 text-sm rounded py-1 px-2 z-60 transition-all duration-200 ease-out font-mono font-semibold tracking-wide h-7 flex items-center justify-center'>
+        <div className='absolute top-[calc(100%+0.35rem)] transform-[translateX(-45%)] bg-gray-950/55 text-zinc-100/90 text-sm rounded py-1 px-2 z-60 transition-all duration-200 ease-out font-mono font-semibold tracking-wide h-auto flex items-center justify-center'>
           {capitalizeFirstLetter(text)}
         </div>
       )}
