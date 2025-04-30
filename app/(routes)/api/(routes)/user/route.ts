8daa936b526f661bc47595c53443f5a449daaa26
clxrityy <clxrityxyz@gmail.com>
2025-04-30 14:03:22 -0400

    add PATCH method

--- app/(routes)/api/(routes)/user/route.ts
+++ app/(routes)/api/(routes)/user/route.ts
@@ -1,4 +1,4 @@
-import { db } from '@/lib';
+import { db, getUserSessionId } from '@/lib';
 import { User } from '@/types';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -26,4 +26,65 @@ export async function GET(req: NextRequest) {
   }
 
   return NextResponse.json({ user }, { status: 200 });
+}
+
+export async function PATCH(req: NextRequest) {
+  const { userId } = await req.json();
+  const sessionId = await getUserSessionId();
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: 'User ID is required',
+      },
+      { status: 400 }
+    );
+  }
+
+  if (!sessionId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
+
+  const user = await db.eUser.findUnique({
+    where: {
+      userId: userId,
+      session: {
+        sessionId: sessionId,
+      }
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        error: "User not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  const updatedUser = await db.eUser.update({
+    where: {
+      userId: userId,
+      session: {
+        sessionId: sessionId,
+      }
+    },
+    data: {
+      ...user,
+      updatedAt: new Date(),
+    }
+  });
+
+  return NextResponse.json(
+    {
+      user: updatedUser,
+    },
+    { status: 200 }
+  );
 }
