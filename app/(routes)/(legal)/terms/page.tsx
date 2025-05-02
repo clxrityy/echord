f0c331fb7bfade6e47755fbbfa87470fedc8bf5b
clxrityy <clxrityxyz@gmail.com>
2025-05-02 12:19:30 -0400

    style

--- app/(routes)/(legal)/terms/page.tsx
+++ app/(routes)/(legal)/terms/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Outline from './outline.mdx';
+import Terms from './terms.mdx';
 
 export default function Page() {
   return (
-    <div>
+    <div className='flex flex-col items-start justify-start w-full gap-3 px-4 py-8 mx-auto max-w-4xl'>
       <Outline />
+      <Terms />
     </div>
   );
 }
