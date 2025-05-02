994b6bdba034fc6ebd58b44ada431e108d4b6dea
clxrityy <clxrityxyz@gmail.com>
2025-05-02 12:49:36 -0400

    add window

--- app/(routes)/(legal)/terms/layout.tsx
+++ app/(routes)/(legal)/terms/layout.tsx
@@ -1,5 +1,8 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import { Metadata } from 'next';
+import Loading from '@/app/loading';
+import { Window } from '@/components/layout';
+import { getUserSessionId } from '@/lib';
 
 export const metadata: Metadata = {
   title: 'Terms of Service',
@@ -13,6 +16,9 @@ export default async function TermsLayout({
   children: ReactNode;
   privacy: ReactNode;
 }) {
+
+  const sessionId = await getUserSessionId();
+
   return (
     <div className='flex flex-col items-start justify-start w-full h-full gap-2 px-4 py-8 mx-auto max-w-6xl max-h-[calc(100vh-4rem)] overflow-y-auto overflow-clip scroll-smooth mt-30'>
       <div className='flex flex-col items-center justify-center w-full gap-2 px-4 py-2 mb-4 text-center'>
@@ -20,11 +26,17 @@ export default async function TermsLayout({
           Terms of Service
         </h1>
         <p>
-          Last updated: <span className='font-semibold'>October 1, 2023</span>
+          Last updated: <span className='font-semibold'>May 2, 2025</span>
         </p>
       </div>
-      {children}
-      {privacy}
+      <Suspense fallback={<Loading />}>
+        <Window sessionId={sessionId ?? ""}>
+          <div className='flex flex-col items-start justify-start w-full h-full gap-2 px-4 py-2 pb-20 text-left -mt-26'>
+            {children}
+            {privacy}
+          </div>
+        </Window>
+      </Suspense>
     </div>
   );
 }
