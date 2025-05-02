2cc388cc1b556cf7ea4212b8e12c2023f0e32215
clxrityy <clxrityxyz@gmail.com>
2025-05-02 12:50:03 -0400

    padding bottom

--- app/(routes)/(legal)/terms/@privacy/page.tsx
+++ app/(routes)/(legal)/terms/@privacy/page.tsx
@@ -2,7 +2,7 @@
 import Privacy from './page.mdx';
 
 export default function Page() {
-  return <div className='max-w-4xl px-4 py-8 mx-auto w-full'>
+  return <div className='max-w-4xl px-4 py-8 mx-auto w-full pb-20'>
     <Privacy />
   </div>;
 }
