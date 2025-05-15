3a3b3a9e50a97618c114df2f4781e95739d58bbf
clxrityy <clxrityxyz@gmail.com>
2025-05-15 15:35:34 -0400

    useInteractions

--- components/categories/interactions/Interact.tsx
+++ components/categories/interactions/Interact.tsx
@@ -1,16 +1,29 @@
 'use client';
 
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { SaveInteraction } from "./SaveInteraction";
 import { FavoriteInteraction } from "./FavoriteInteraction";
 import { RatingInteraction } from "./RatingInteraction";
+import { useInteractions } from "@/contexts";
+import { Interaction } from "@/types";
 
 interface Props {
   userId: string;
   trackId: string;
+  initialInteractions: Interaction[] | []
 }
 
-export function Interact({ userId, trackId }: Readonly<Props>) {
+export function Interact({ userId, trackId, initialInteractions }: Readonly<Props>) {
+
+  const { setInteractions, getInteractions } = useInteractions();
+
+  useEffect(() => {
+    if (initialInteractions.length > 0 && getInteractions() === undefined) {
+      setInteractions(initialInteractions);
+    }
+
+  }, [initialInteractions, setInteractions, getInteractions])
+
   return <div className="flex flex-col items-center justify-center gap-5">
     <div className="flex items-center gap-2">
       <Suspense>
