aca0fbcd6757117daa95852439b00a637efd98d7
clxrityy <clxrityxyz@gmail.com>
2025-05-21 23:12:12 -0400

    icons

--- components/ui/Pagination.tsx
+++ components/ui/Pagination.tsx
@@ -104,15 +104,20 @@ export function Pagination({
     return pages;
   };
 
+  const {
+    left: IconLeft,
+    right: IconRight,
+  } = ICONS;
+
   return (
-    <div className='flex items-center justify-center gap-4 py-6 mb-10 xl:mb-4 font-[var(--font-tmr)]'>
+    <div className='flex items-center justify-center gap-4 py-6 mb-10 xl:mb-4 font-[var(--font-tmr)] relative z-10'>
       <Button
         onClick={() => handlePageChange(page - 1)}
         disabled={page === 1}
         aria-label='Previous Page'
         className='transition-colors duration-200 ease-in-out enabled:hover:scale-105'
       >
-        <ICONS.left className='h-4 w-4 xl:h-6 xl:w-6' />
+        <IconLeft className='h-4 w-4 xl:h-6 xl:w-6' />
       </Button>
       <div className='flex items-center gap-2 font-semibold'>
         {renderPageNumbers()}
@@ -123,7 +128,7 @@ export function Pagination({
         aria-label='Next Page'
         className='transition-colors duration-200 ease-in-out enabled:hover:scale-105'
       >
-        <ICONS.right className='h-4 w-4 xl:w-6 xl:h-6' />
+        <IconRight className='h-4 w-4 xl:w-6 xl:h-6' />
       </Button>
     </div>
   );
