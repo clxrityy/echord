bd3ded37426a45b084de095d393125bec66dcbe1
clxrityy <clxrityxyz@gmail.com>
2025-05-21 23:11:57 -0400

    add user feed

--- app/(routes)/(main)/profile/[id]/page.tsx
+++ app/(routes)/(main)/profile/[id]/page.tsx
@@ -4,6 +4,9 @@ import { Window } from '@/components/layout';
 import Loading from '@/app/loading';
 import { db, getUserSessionId } from '@/lib';
 import { Skeleton } from '@/components/ui';
+import { FeedItem, FeedList, FeedListItem, FeedListItemSkeleton } from '@/components/categories/feed';
+import { Interaction } from '@/types';
+import { v4 as uuid } from 'uuid';
 
 type Props = {
   params: Promise<{
@@ -155,6 +158,46 @@ export default async function Page({ params }: Props) {
     starsGiven: await getRatings(),
   };
 
+  async function getUserFeed(): Promise<Interaction[]> {
+    const allInteractions = await db.eInteractionData.findMany({
+      include: {
+        eData: true,
+      }
+    });
+
+    const userInteractions = await db.eInteraction.findMany({
+      where: {
+        userId: profileUser?.userId,
+      },
+      include: {
+        eData: true,
+        user: true,
+        eAlbum: true,
+        eTrack: true
+      },
+    });
+
+    const interactions = allInteractions.filter((interaction) => {
+      return userInteractions.some(
+        (userInteraction) => userInteraction.eData.id === interaction.dataId
+      );
+    });
+
+    const userFeed = userInteractions.map((interaction) => {
+      const foundInteraction = interactions.find(
+        (i) => i.dataId === interaction.eData.id
+      );
+      return {
+        ...interaction,
+        interactionData: foundInteraction ?? null,
+      };
+    });
+
+    return userFeed;
+  }
+
+  const userFeed = await getUserFeed();
+
   return (
     <Suspense fallback={<Loading />}>
       <Window sessionId={sessionId || ''}>
@@ -174,6 +217,69 @@ export default async function Page({ params }: Props) {
            */}
           <div className='md:absolute w-full flex flex-col items-center justify-center gap-2'>
             <Favorites interactionData={await getFavorites()} />
+          </div>
+          <div className='w-full flex items-center max-w-screen-sm h-full'>
+            <Suspense>
+              <FeedList itemsPerPage={5}>
+                {userFeed.map((item) => {
+                  const interaction = {
+                    ...item!,
+                  }
+
+                  const data = {
+                    id: item?.dataId,
+                    albumId: item?.albumId,
+                    trackId: item?.trackId,
+                  };
+
+                  const interactionData = {
+                    ...item?.interactionData,
+                    id: item?.interactionData?.id!,
+                    dataId: item?.interactionData?.dataId!,
+                    createdAt: item?.interactionData?.createdAt!,
+                    interactionType: item?.interactionData?.interactionType!,
+                    updatedAt: item?.interactionData?.updatedAt!,
+                    title: item?.interactionData?.title!,
+                    targetUserId: item?.interactionData?.targetUserId ?? null,
+                    imageUrl: item?.interactionData?.imageUrl ?? null,
+                    artistName: item?.interactionData?.artistName ?? null,
+                    albumName: item?.interactionData?.albumName ?? null,
+                    rating: item?.interactionData?.rating ?? null,
+                    review: item?.interactionData?.review ?? null,
+                    albumId: item?.interactionData?.albumId ?? null,
+                    trackId: item?.interactionData?.trackId ?? null,
+                  };
+
+                  const userData = item?.user;
+
+                  return (
+                    <Suspense
+                      key={uuid()}
+                      fallback={
+                        <FeedListItemSkeleton key={item?.user.id} />
+                      }
+                    >
+                      <FeedListItem key={item?.user.userId}>
+                        {interaction &&
+                          data &&
+                          interactionData &&
+                          userData ? (
+                          <FeedItem
+                            interaction={interaction}
+                            // data={data}
+                            interactionData={interactionData}
+                            userId={userData.userId}
+                            username={
+                              userData.username ? userData.username : null
+                            }
+                          />
+                        ) : null}
+                      </FeedListItem>
+                    </Suspense>
+                  );
+                })}
+              </FeedList>
+            </Suspense>
           </div>
         </div>
         <SavesGrid saves={await getSaves()} />
