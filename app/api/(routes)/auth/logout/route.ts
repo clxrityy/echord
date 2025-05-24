e9e0f4135c4e1faf66e6e56d0fa2ac61616e2178
clxrityy <clxrityxyz@gmail.com>
2025-05-24 17:09:49 -0400

    fix logout

--- app/api/(routes)/auth/logout/route.ts
+++ app/api/(routes)/auth/logout/route.ts
@@ -1,11 +1,13 @@
 import { ENV } from '@/util';
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export async function POST(_req: NextRequest) {
+
+  const cookieStore = await cookies();
+
   try {
-    const res = NextResponse.next();
-    res.cookies.delete(ENV.COOKIE_NAME);
-    res.cookies.set(ENV.COOKIE_NAME, '', {
+    cookieStore.set(ENV.COOKIE_NAME, '', {
       name: ENV.COOKIE_NAME,
       value: '',
       expires: new Date(0),
@@ -14,10 +16,16 @@ export async function POST(_req: NextRequest) {
       secure: process.env.NODE_ENV === 'production',
       maxAge: 0,
     });
+    cookieStore.delete(ENV.COOKIE_NAME);
 
     return NextResponse.json(
       { message: 'Logged out successfully.' },
-      { status: 200 }
+      {
+        status: 200,
+        headers: {
+          'Set-Cookie': `${ENV.COOKIE_NAME}=; Max-Age=0; Path=/; HttpOnly; SameSite=Lax; Secure=${process.env.NODE_ENV === 'production'}`,
+        }
+       },
     );
   } catch (error) {
     console.error('Error in logout route:', error);
