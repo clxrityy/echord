812a684ec2a9b8bdbed4a9c36bb1a7778fdd9589
clxrityy <clxrityxyz@gmail.com>
2025-05-24 14:56:53 -0400

    add interactions to modal

--- components/categories/modals/AlbumModal.tsx
+++ components/categories/modals/AlbumModal.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { DialogProps, Dialog, ImageComponent } from '@/components/ui';
+import Link from 'next/link';
 import { Suspense } from 'react';
+import { Interact } from '../interactions';
+import { useInteractions, useSession } from '@/contexts';
 
 interface AlbumModalProps extends DialogProps {
   albumId: string | null;
@@ -14,14 +18,20 @@ interface AlbumModalProps extends DialogProps {
 export function AlbumModal({
   onClose,
   open,
-  // albumId,
+  albumId,
   title,
   description,
   albumName,
   imageUrl,
   artistName,
-  // trackId,
+  trackId,
 }: Readonly<AlbumModalProps>) {
+
+  const { userId } = useSession();
+
+  const { getInteractions } = useInteractions();
+  const initialInteractions = getInteractions() ?? [];
+
   return (
     <Suspense>
       <Dialog
@@ -41,10 +51,27 @@ export function AlbumModal({
               height={200}
             />
           )}
-          <h4 className='text-xl font-semibold mt-2'>{albumName}</h4>
+          <div className='flex flex-col items-center justify-center my-2 gap-4'>
+            <Link href={`/track/${trackId}`} className='hover:underline hover:text-gray-300 focus:underline-offset-2 transition-all duration-200 ease-in'>
+              <h2>
+                {title}
+              </h2>
+            </Link>
+            <h4 className='text-xl font-semibold text-gray-300'>{albumName}</h4>
+          </div>
           <p className='text-gray-600'>{artistName}</p>
         </div>
-        <div className='flex justify-center flex-col p-4'></div>
+        <div className='flex justify-center flex-col items-center p-4'>
+          {
+            trackId && userId && (
+              <Interact
+                trackId={trackId}
+                userId={userId}
+                initialInteractions={initialInteractions}
+              />
+            )
+          }
+        </div>
       </Dialog>
     </Suspense>
   );
